4a103ec0fd80e15557f2a63c45e3617672b745d4
Thiago Galindo <thiagogalindo33@gmail.com>
2024-02-27 20:05:43 -0300

    adicionando arquivo script

---
+++ Exercícios de lógica de programação/c#/Two Sum/Two Sum.cs
@@ -0,0 +1,29 @@
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int counter1 = 0;
+        int counter2 = 0;
+        bool conditionMet = false;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 1; j <= (nums.Length - 1); j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    counter1 = i;
+                    counter2 = j;
+                    conditionMet = true;
+                    break;
+                }
+            }
+            if (conditionMet == true)
+            {
+                break;
+            }
+        }
+        int[] result = [cont1, cont2];
+        return result;
+    }
+}
